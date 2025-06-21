dfd4c1db794513ea30ff9b41f93ec72880a5d8a9
GaromaMakure <garomamak11@gmail.com>
2025-06-21 07:56:16 -0700

    updating Book cover

--- components/BookCover.tsx
+++ components/BookCover.tsx
@@ -1,7 +1,12 @@
-import { cn } from "@/lib/utils";
+"use client";
+
 import React from "react";
-import  Image from 'next/image';
+import { cn } from "@/lib/utils";
+import BookCoverSvg from "@/components/BookCoverSvg";
+import Image from "next/image";
+
 type BookCoverVariant = "extraSmall" | "small" | "medium" | "regular" | "wide";
+
 const variantStyles: Record<BookCoverVariant, string> = {
   extraSmall: "book-cover_extra_small",
   small: "book-cover_small",
@@ -9,18 +14,20 @@ const variantStyles: Record<BookCoverVariant, string> = {
   regular: "book-cover_regular",
   wide: "book-cover_wide",
 };
-interface props {
+
+interface Props {
   className?: string;
   variant?: BookCoverVariant;
-  coverColor: string;
-  coverImage: string;
+  color: string;
+  cover: string;
 }
+
 const BookCover = ({
   className,
   variant = "regular",
-  coverColor = "#012B48",
-  coverImage = "https://placehold.co/400x600.png",
-}: props) => {
+  color = "#012B48",
+  cover = "https://placehold.co/400x600.png",
+}: Props) => {
   return (
     <div
       className={cn(
@@ -29,11 +36,18 @@ const BookCover = ({
         className
       )}
     >
-      BOOK SIDE SVG
-      <div className="absolute z-10" style = {{left: '12%', width: "87.5%", height:"88%"}} >
-        <Image src = {coverImage} alt = "book cover"  fill className = "rounded-sm object-fill " />
+      <BookCoverSvg coverColor={color} />
 
-
+      <div
+        className="absolute z-10"
+        style={{ left: "12%", width: "87.5%", height: "88%" }}
+      >
+        <Image
+          src={cover}
+          alt="book cover"
+          fill
+          className="rounded-sm object-fill"
+        />
       </div>
     </div>
   );
